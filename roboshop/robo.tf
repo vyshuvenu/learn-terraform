ccbce8728362d2ff4441fa433cae925834ef9132
vyshuvenu <vyshuv1289@gmail.com>
2023-08-04 16:05:13 +0530

    error corrected

--- roboshop/robo.tf
+++ roboshop/robo.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "frontend" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
   vpc_security_group_ids = [ "sg-0af3e5a83f971329a" ]
   tags = {
@@ -7,7 +7,7 @@ resource "aws_instance" "frontend" {
   }
 }
 resource "aws_route53_record" "frontend" {
-zone_id = Z02452941A9F
+zone_id = "Z02452941A9F"
 name    = "frontend.vyshu.online"
 type    = "A"
 ttl     = 30
