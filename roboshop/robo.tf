9e5ce753a46115db75d5eb87c8841dce6dc05e3e
vyshuvenu <vyshuv1289@gmail.com>
2023-08-10 14:33:55 +0530

    error correct

--- roboshop/robo.tf
+++ roboshop/robo.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
-  vpc_security_group_ids = "sg-08aa5756a692da5db"
+  vpc_security_group_ids = [ "sg-08aa5756a692da5db" ]
 
   tags = {
     Name = "frontend"
