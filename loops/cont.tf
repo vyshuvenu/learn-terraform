5a68418fbf9554c05f5ef4fcd2e392fd6e54d4c2
vyshuvenu <vyshuv1289@gmail.com>
2023-08-10 16:05:09 +0530

    loops created

---
+++ loops/cont.tf
@@ -0,0 +1,22 @@
+variable "components" {
+  default = [ "catalogue", "frontend", "mongodb" ]
+}
+
+//resource "aws_instance" "instance" {
+//
+//  count = length(var.components)
+//
+//  ami           = "ami-03265a0778a880afb"
+//  instance_type = "t3.small"
+//  vpc_security_group_ids = [ "sg-0dee954b08055e577" ]
+//
+//  tags = {
+//    //Name = var.components[count.index]
+//    Name = element(var.components, count.index)
+//  }
+//}
+
+resource "aws_security_group" "allow_tls" {
+  count = length(var.components)
+  name        = element(var.components, count.index)
+}
---
+++ loops/foreach.tf
@@ -0,0 +1,14 @@
+variable "components" {
+  default = {
+    frontend = { name = "frontend_sg" }
+    catalogue = { name = "catalogue_sg" }
+    mongodb = { name = "mongodb_sg"  }
+  }
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  for_each = var.components
+  name        = lookup(var.components, each.value["name"], null)
+}
+
