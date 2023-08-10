f308c99698f216cf8a2e2f645f212d2b55d9fc7e
vyshuvenu <vyshuv1289@gmail.com>
2023-08-10 15:23:36 +0530

    error correct

---
+++ funtions/function.tf
@@ -0,0 +1,25 @@
+variable "fruits" {
+  default = [ "apple", "banana"]
+}
+
+output "fruit" {
+  //value = var.fruits[2]
+  value  = element(var.fruits, 2)
+}
+
+variable "fruit_with_stock" {
+  default = {
+    apple = {
+      stock = 100
+    }
+  }
+}
+
+output "fruit_stock" {
+  value  = try(var.fruit_with_stock["banana"], 0)
+}
+
+output "fruit_stock_price" {
+  value  = lookup(var.fruit_with_stock["apple"], "price", 2)
+  // try(var.fruit_with_stock["apple"].price, 2)
+}
