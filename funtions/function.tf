f524f10c85f395f570b186edbfadb7c9c3fce507
vyshuvenu <vyshuv1289@gmail.com>
2023-08-10 15:55:11 +0530

    error correct

--- funtions/function.tf
+++ funtions/function.tf
@@ -16,7 +16,7 @@ variable "fruit_with_stock" {
 }
 
 output "fruit_stock" {
-  value  = try(var.fruit_with_stock["banana"], 0)
+  value  = try(var.fruit_with_stock["apple"], 0)
 }
 
 output "fruit_stock_price" {
