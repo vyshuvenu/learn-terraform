7cd24189f296794c89a9d594b19d9c8798002395
vyshuvenu <vyshuv1289@gmail.com>
2023-08-11 17:02:56 +0530

    error fixed

--- robo-v3/module/main.tf
+++ robo-v3/module/main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "instance" {
 resource "aws_route53_record" "record" {
 
   zone_id = var.zone_id
-  name    = "$(var.name)-dev.vyshu.online"
+  name    = "${var.name}-dev.vyshu.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.public_ip ]
@@ -24,7 +24,7 @@ resource null_resource "ansible" {
   depends_on = [
     aws_route53_record.record
   ]
-  provisioner "local-exec" "ansible" {
+  provisioner "local-exec" {
     command = <<EOF
 cd /home/centos/robo-ansible-roles
 git pull
