fb6eb5df0f7cdc0af3712eb5dcc4c8793a2fcda9
vyshuvenu <vyshuv1289@gmail.com>
2023-08-11 16:40:41 +0530

    robo_v3

--- robo-v3/module/main.tf
+++ robo-v3/module/main.tf
@@ -14,12 +14,24 @@ resource "aws_instance" "instance" {
 resource "aws_route53_record" "record" {
 
   zone_id = var.zone_id
-  name    = "$(var.name).vyshu.online"
+  name    = "$(var.name)-dev.vyshu.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.public_ip ]
 }
 
+resource null_resource "ansible" {
+  depends_on = [
+    aws_route53_record.record
+  ]
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/robo-ansible-roles
+git pull
+ansible-playbook -i ${var.name}-dev.vyshu.online, main.yml -e "ansible_user=centos ansible_password=DevOps321" component=${var.name}
+EOF
+  }
+}
 
 
 
