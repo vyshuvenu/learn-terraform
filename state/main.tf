a0e595874dd8458740403511ef54fdee116dc4bb
vyshuvenu <vyshuv1289@gmail.com>
2023-08-13 08:51:19 +0530

    new commit

---
+++ state/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket = "tf-state-db100"
+    key    = "09-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+variable "test" {
+  default = "Hello"
+}
+
+output "test" {
+  value = var.test
+}
